d4fcf577d70a45215ae194627c1ae712bf6a9f77
Negar Yousefian <60235642+NegarYousefian@users.noreply.github.com>
2023-12-07 18:19:41 +0200

    Update math_library_WASM.js

--- math_library_WASM.js
+++ math_library_WASM.js
@@ -10,21 +10,27 @@ async function loadWebAssembly() {
         const module = await WebAssembly.instantiate(buffer, {}); // Instantiate the WebAssembly module
 
         // Get the exports (functions) from the WebAssembly instance
-        const { fibonacci, factorial, approximatePi } = module.instance.exports;
+        const { fibonacci, factorial, approximatePi, isPrime, ackermann, zetaFunction } = module.instance.exports;
 
-        const n1 = 5;
-        const n2 = 100;
-        const result1 = fibonacci(n1);
-        const result2 = factorial(n1);
-        const result3 = approximatePi(n2);
-
-        console.log(`Fibonacci number at position ${n1}: ${result1}`);
-        console.log(`Factorial of ${n1}: ${result2}`);
-        console.log(`Pi approximation after ${n2} iterations: ${result3}`);
+        const inputs = [
+            {i1: 30}, // fibonacci
+            {i1: 20}, // factorial
+            {i1: 1000}, // approximate pi
+            {i1: 2147483647}, // is prime
+            {i1: 3, i2: 6}, // ackermann
+            {i1: 1.000001, i2:1000000}, // zeta
+        ];
+        const functions = [fibonacci, factorial, approximatePi, isPrime, ackermann, zetaFunction];
+        functions.forEach((fn, index) => {
+        const startTime = performance.now();
+        const result = inputs[index].i2 ? fn(inputs[index].i1, inputs[index].i2) : fn(inputs[index].i1);
+        console.log('output: ', result);
+        console.log('time: ', performance.now() - startTime);
+        console.log('_________________________________');
+        });
     } catch (error) {
         console.error('Error loading WebAssembly module:', error);
     }
 }
 
 loadWebAssembly();
-
